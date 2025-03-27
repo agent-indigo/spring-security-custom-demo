ee43d3231b2eaeab7388a0f9c7dc48e1364dfc0b
agent-indigo <bradenhynes@gmail.com>
2025-03-27 13:46:39 -0230

    Code cleanup

--- src/main/java/com/keyin/hynes/braden/spring/security/custom/demo/abstracts/DataEntity.java
+++ src/main/java/com/keyin/hynes/braden/spring/security/custom/demo/abstracts/DataEntity.java
@@ -2,10 +2,10 @@ package com.keyin.hynes.braden.spring.security.custom.demo.abstracts;
 import java.util.UUID;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
+import lombok.Data;
 @MappedSuperclass
+@Data
 public abstract class DataEntity {
   @Id
-  @Getter
   private final UUID id = UUID.randomUUID();
 }
--- src/main/java/com/keyin/hynes/braden/spring/security/custom/demo/entities/UserEntity.java
+++ src/main/java/com/keyin/hynes/braden/spring/security/custom/demo/entities/UserEntity.java
@@ -6,18 +6,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 @Entity
 @Table(name = "users")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
 public final class UserEntity extends DataEntity implements UserDetails {
   @Column(unique = true)
   private String username;
